e11ff4adf6db3b7b0d05f5700d228aabbd5f3ae5
Cherkesky <guy@cherkesky.com>
2021-01-07 16:47:57 -0600

    pagination

--- get_data.py
+++ get_data.py
@@ -1,15 +1,47 @@
 import requests
-from query import query
+from query import block
 from secret import token
 
+startCursor = ""
+endCursor = ""
 headers = {"Authorization": token}
+hasNextPage = "True"
 
-def run_query(query): 
+def run_query(endCursor=""): 
+    if endCursor == "":
+        searchQuery = 'search(query: "location:Nashville", type: USER, first: 3)'
+    else:
+        searchQuery = f'search( first: 3, after: "{endCursor}", query: "location:Nashville", type: USER)'
+    
+    query = f"""
+    {{
+    {searchQuery}
+    {block}
+    }}
+    """
+
     request = requests.post('https://api.github.com/graphql', json={'query': query}, headers=headers)
     if request.status_code == 200:
         return request.json()
     else:
         raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
-           
-result = run_query(query) 
-print (result)
+
+
+
+def parse_query():
+    
+    result = run_query() 
+    endCursor = result["data"]["search"]["pageInfo"]["endCursor"]
+    startCursor = result["data"]["search"]["pageInfo"]["startCursor"]
+    hasNextPage =  str(result["data"]["search"]["pageInfo"]["hasNextPage"])
+    print (result)
+    print (startCursor, endCursor, hasNextPage)  
+    while hasNextPage == 'True':
+        result = run_query(endCursor) 
+        endCursor = result["data"]["search"]["pageInfo"]["endCursor"]
+        startCursor = result["data"]["search"]["pageInfo"]["startCursor"]
+        hasNextPage =  str(result["data"]["search"]["pageInfo"]["hasNextPage"])
+        print (result)
+        print (startCursor, endCursor, hasNextPage)
+
+parse_query()
--- query.py
+++ query.py
@@ -1,6 +1,7 @@
 query = """
 {
-search(query: "location:Nashville", type: USER, first: 100) {
+search(query: "location:Nashville", type: USER, first: 100) 
+{
     userCount
     edges {
       node {
@@ -30,3 +31,34 @@ search(query: "location:Nashville", type: USER, first: 100) {
   }
 }
 """
+
+block = """
+{
+    userCount
+    edges {
+      node {
+        ... on User {
+          name
+          login
+          location
+          repositories(last: 10) {
+            totalCount
+            nodes {
+              id
+              languages(first: 5) {
+                nodes {
+                  name
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    pageInfo {
+      endCursor
+      startCursor
+      hasNextPage
+    }
+  }
+  """
